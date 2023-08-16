288e154c11852a6f268a94da66856dc37fbc22d9
amarquardt <34435039+amarquardt@users.noreply.github.com>
2023-08-16 12:48:19 -0700

    Update delayed.js
    add analytics.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
+import Analytics from './analytics.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
