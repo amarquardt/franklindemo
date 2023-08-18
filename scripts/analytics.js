36f58db38134b8b8ab4c23d6136b5f7bada2c90d
amarquardt <34435039+amarquardt@users.noreply.github.com>
2023-08-17 17:45:38 -0700

    Update analytics.js
    Add adobe data layer push

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -1,2 +1,17 @@
 
 console.log("test");
+
+//Adobe Client Data Layer
+adobeDataLayer.push({
+  "event":"top of page",
+  "page":{
+    "name":"blog"
+  }
+});
+
+adobeDataLayer.push({
+  "event":"bottom of page",
+  "page":{
+    "type":"article"
+  }
+});
