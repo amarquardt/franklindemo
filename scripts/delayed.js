0fc23a8491ab3807705c45c140baeac73724a310
amarquardt <34435039+amarquardt@users.noreply.github.com>
2023-08-17 17:27:43 -0700

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
+import "./adobe-client-data-layer.min.js";
 import "./analytics.js";
 
 // Core Web Vitals RUM collection
