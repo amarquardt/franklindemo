3d65b6ac671ddadec33fb13c5b5148d93c4bf1cb
Marquardt, Alan <Marquardt.Alan@mayo.edu>
2023-08-22 16:53:07 -0700

    sync

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
-import "./adobe-client-data-layer.min.js";
 import "./analytics.js";
 
 // Core Web Vitals RUM collection
